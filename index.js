ea520ce9a5c56f6006b6e669a6fb1a797f097981
Sumit <abcddab1@gmail.com>
2026-08-20 18:43:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,9 @@ app.command("/keni-help", async ({ ack, respond }) => {
     text:
 `Available Commands:
 /keni-hi : Makes a greeting
-/keni-catfact : Tells a fact about cats`
+/keni-catfact : Tells a fact about cats
+/keni-coinflip : Heads or tails idk
+/keni-dice : Rolls a dice`
   });
 });
 
@@ -35,6 +37,24 @@ app.command("/keni-catfact", async ({ ack, respond }) => {
   } catch (err) {
     await respond({ text: "Failed to fetch a cat fact." });
   }
+});
+
+app.command("/keni-coinflip", async ({ack, respond }) => {
+  await ack();
+  const result = Math.random() < 0.5 ? "Heads" : "Tails";
+  await respond(`The coin landed on ${result}!`);
+  });
+
+app.command("/keni-dice", async ({ ack, respond }) => { 
+  await ack();
+  const roll= Math.floor(Math.random() *6)+1;
+  await respond(`I rolled a ${roll}!`);
+});
+
+app.command("/keni-pick", async ({command, ack, respond }) => {
+  await ack();
+  
+
 });
 
 (async () => {
